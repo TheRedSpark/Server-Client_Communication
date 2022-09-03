409ef3b1a6d146aaa58b603b889b9a73015a6736
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-03 23:02:13 +0200

    Simple Connection

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,8 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, client!");
+    let mut conn = std::net::TcpStream::connect("localhost:8000").unwrap();
+
+    conn.write(b"Hello Server").unwrap();
+    conn.flush().unwrap();
 }
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,9 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, Server!");
+    let listener = std::net::TcpListener::bind("localhost:8000").unwrap();
+    let mut conn = listener.accept().unwrap();
+    let mut buf = String::new();
+    conn.0.read_to_string(&mut buf).unwrap();
+    println!("{buf}");
 }
