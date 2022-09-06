ea59af13c50d94e2423cb084a8a8d65bf931bf60
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-06 15:41:42 +0200

    Server kann Daten nun empfangen

--- src/client/main.rs
+++ src/client/main.rs
@@ -6,24 +6,31 @@ use std::str;
 
 fn main() {
     use std::net::{Shutdown, TcpStream};
-    let mut buf = [0 as u8; 32];
+    let mut buf = [0 as u8; 48];
     let client_id = b"c0000001";
     let echo = b"jejdkztb";
-    let data = [0 as u8; 16];
-    let mut command = [0 as u8; 16];
+    //let mut data = [0 as u8; 16];
+    //data = *b"0000000000000000";
+    let data= b"0000000000000001";
+    //let mut command = [0 as u8; 16];
+    //command = *b"0000000000000000";
+    let command= b"0000000000000001";
     let mut i = 0;
-    while i < 30 {
+    while i < 1 {
         let mut stream = TcpStream::connect("127.0.0.1:80")
             .expect("Couldn't connect to the server...");
         println!("Connectet");
         stream.write(echo).unwrap();
+        stream.write(client_id).unwrap();
+        stream.write(data).unwrap();
+        stream.write(command).unwrap();
         stream.read(&mut buf).unwrap();
         if &buf[0..8] == echo {
-            println!("Echo complete");
-            stream.write(client_id).unwrap();
+            //println!("Echo complete");
+
         }
         i += 1;
-        println!("{}",i);
+        //println!("{}",i);
         stream.read(&mut buf).unwrap();
         println!("Der Befehl lautet: {}",str::from_utf8(&buf).unwrap());
         stream.shutdown(Shutdown::Both).expect("shutdown call failed");
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,14 +1,25 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::str::from_utf8;
+use std::str;
 
 fn handle_client(mut stream: TcpStream) {
-    println!("Client connectet");
-    let mut data = [0 as u8; 50];
-    stream.read(&mut data).unwrap();
-    stream.write(&data).unwrap();
+    let mut buff = [0 as u8; 48];
+    stream.read(&mut buff).unwrap();
+    let echo = str::from_utf8(&buff[0..8]).unwrap();
+    let client_id = str::from_utf8(&buff[8..16]).unwrap();
+    let data = str::from_utf8(&buff[16..32]).unwrap();
+    let command = str::from_utf8(&buff[32..48]).unwrap();
+    println!("Echo is :{}", echo);
+    println!("Client {} ist verbunden", client_id);
+    println!("Data is: {}", data);
+    println!("Command is: {}", command);
+
+
+    stream.write((&buff).as_ref()).unwrap();
     //stream.write(b"Herunterfahren").unwrap();
-    println!("Echo send wait for Reply")
+    //println!("Echo send wait for Reply")
 }
 
 fn main() -> std::io::Result<()> {
