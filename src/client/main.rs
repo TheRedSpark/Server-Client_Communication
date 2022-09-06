883c6f9297517723b15ed38abd92440ddf0bb7c2
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-06 12:17:24 +0200

    Vorbereitete Wiederholte Datenübertragung

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,21 +1,32 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
+//use std::simd::u8x16;
 use std::str::from_utf8;
 use std::str;
 
 fn main() {
     use std::net::{Shutdown, TcpStream};
-
-    let mut stream = TcpStream::connect("127.0.0.1:80")
-        .expect("Couldn't connect to the server...");
-    println!("Connectet");
-    let mut buf = [0 as u8; 4];
-    let echo = b"Echo";
-    stream.write(echo).unwrap();
-    stream.read(&mut buf).unwrap();
-    if &buf == echo {
-        println!("Echo complete")
+    let mut buf = [0 as u8; 32];
+    let client_id = b"c0000001";
+    let echo = b"jejdkztb";
+    let data = [0 as u8; 16];
+    let mut command = [0 as u8; 16];
+    let mut i = 0;
+    while i < 30 {
+        let mut stream = TcpStream::connect("127.0.0.1:80")
+            .expect("Couldn't connect to the server...");
+        println!("Connectet");
+        stream.write(echo).unwrap();
+        stream.read(&mut buf).unwrap();
+        if &buf[0..8] == echo {
+            println!("Echo complete");
+            stream.write(client_id).unwrap();
+        }
+        i += 1;
+        println!("{}",i);
+        stream.read(&mut buf).unwrap();
+        println!("Der Befehl lautet: {}",str::from_utf8(&buf).unwrap());
+        stream.shutdown(Shutdown::Both).expect("shutdown call failed");
     }
-    println!("Der Befehl lautet: {}",str::from_utf8(&buf).unwrap());
-    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+
 }
