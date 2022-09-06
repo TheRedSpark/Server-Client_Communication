8e71537208df2f3d8fcc40230515a6130ca0eb47
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-06 16:54:22 +0200

    Server kann Daten nun verarbeiten und antworten

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,34 +1,33 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
 //use std::simd::u8x16;
-use std::str::from_utf8;
 use std::str;
 
 fn main() {
     use std::net::{Shutdown, TcpStream};
     let mut buf = [0 as u8; 48];
     let client_id = b"c0000001";
-    let echo = b"jejdkztb";
+    let echo_key = b"jejdkztb";
     //let mut data = [0 as u8; 16];
     //data = *b"0000000000000000";
-    let data= b"0000000000000001";
+    let data= b"D000000000000003";
     //let mut command = [0 as u8; 16];
     //command = *b"0000000000000000";
-    let command= b"0000000000000001";
+    let command= b"C000000000000001";
     let mut i = 0;
     while i < 1 {
         let mut stream = TcpStream::connect("127.0.0.1:80")
             .expect("Couldn't connect to the server...");
         println!("Connectet");
-        stream.write(echo).unwrap();
+        stream.write(echo_key).unwrap();
         stream.write(client_id).unwrap();
         stream.write(data).unwrap();
         stream.write(command).unwrap();
-        stream.read(&mut buf).unwrap();
-        if &buf[0..8] == echo {
+        //stream.read(&mut buf).unwrap();
+        /*if &buf[0..8] == echo_key {
             //println!("Echo complete");
 
-        }
+        }*/
         i += 1;
         //println!("{}",i);
         stream.read(&mut buf).unwrap();
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,25 +1,38 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
-use std::str::from_utf8;
+//use std::str::from_utf8;
 use std::str;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buff = [0 as u8; 48];
     stream.read(&mut buff).unwrap();
-    let echo = str::from_utf8(&buff[0..8]).unwrap();
-    let client_id = str::from_utf8(&buff[8..16]).unwrap();
+    let echo_key = str::from_utf8(&buff[0..8]).unwrap();
+    let client_id: &str = str::from_utf8(&buff[8..16]).unwrap();
     let data = str::from_utf8(&buff[16..32]).unwrap();
     let command = str::from_utf8(&buff[32..48]).unwrap();
-    println!("Echo is :{}", echo);
+    println!("Echo is :{}", echo_key);
     println!("Client {} ist verbunden", client_id);
     println!("Data is: {}", data);
     println!("Command is: {}", command);
-
-
-    stream.write((&buff).as_ref()).unwrap();
+    let data = handle_data(data.as_bytes());
+    let response: String = format!("{}{}{}{}", echo_key, client_id, data, command);
+    stream.write(response.as_ref()).unwrap();
+    // stream.write(client_id.as_ref()).unwrap();
+    // stream.write(data.as_ref()).unwrap();
+    // stream.write(command.as_ref()).unwrap();
     //stream.write(b"Herunterfahren").unwrap();
     //println!("Echo send wait for Reply")
+}
+
+fn handle_data(incomming_data: &[u8]) -> String {
+    let data:[u8;16] = *match incomming_data {
+        b"D000000000000001" => b"D000000000000002",
+        &_ => b"Der000005555ror1"
+    };
+    let mut s = String::new();
+    s = str::from_utf8(&data).unwrap().parse().unwrap();
+    return s;
 }
 
 fn main() -> std::io::Result<()> {
