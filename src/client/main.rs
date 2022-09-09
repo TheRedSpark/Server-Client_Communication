c09311eea0c9bd12572c8a6d9b46be83c175db9f
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-09 17:56:47 +0200

    Errorhandling

--- src/client/main.rs
+++ src/client/main.rs
@@ -20,28 +20,17 @@ fn communication() -> Result<String, Error> {
     //command = *b"0000000000000000";
     let command = b"C000000000000001";
     let mut i = 0;
-    while i < 10 {
-        //let mut stream = TcpStream::connect("127.0.0.1:80")
-        //    .expect("Couldn't connect to the server...");
-        let mut stream = TcpStream::connect("10.0.5.152:80")
-            .expect("Couldn't connect to the server...");
-        println!("Connectet");
-        stream.write(echo_key).unwrap();
-        stream.write(client_id).unwrap();
-        stream.write(data).unwrap();
-        stream.write(command).unwrap();
-        //stream.read(&mut buf).unwrap();
-        /*if &buf[0..8] == echo_key {
-            //println!("Echo complete");
-
-        }*/
-        i += 1;
-        //println!("{}",i);
-        stream.read(&mut buf).unwrap();
-        println!("Der Befehl lautet: {}", str::from_utf8(&buf).unwrap());
-        stream.shutdown(Shutdown::Both).expect("shutdown call failed");
-        thread::sleep(time::Duration::from_millis(5000));
-    }
+    let mut stream = TcpStream::connect("10.0.5.152:80")
+        .expect("Couldn't connect to the server...");
+    println!("Connectet");
+    stream.write(echo_key).unwrap();
+    stream.write(client_id).unwrap();
+    stream.write(data).unwrap();
+    stream.write(command).unwrap();
+    stream.read(&mut buf).unwrap();
+    println!("Der Befehl lautet: {}", str::from_utf8(&buf).unwrap());
+    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+    thread::sleep(time::Duration::from_millis(5000));
     let s = "Hat geklappt";
     Ok(s.parse().unwrap())
 }
