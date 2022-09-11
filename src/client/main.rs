355d64969a33f070c2e9496d3cabc9e17ec07702
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-11 21:01:28 +0200

    -

--- src/client/main.rs
+++ src/client/main.rs
@@ -8,15 +8,14 @@ use std::str;
 use std::{thread, time};
 use std::fmt::Error as OtherError;
 
-fn communication() -> Result<String, Error> {
+fn communication() -> Result<_, _> {
     use std::net::{Shutdown, TcpStream};
     let mut buf = [0 as u8; 48];
     let client_id = b"c0000001";
     let echo_key = b"jejdkztb";
     let data = b"D000000000000003";
     let command = b"C000000000000001";
-    let mut i = 0;
-    let mut stream = TcpStream::connect("10.0.5.152:80")
+    let mut stream = TcpStream::connect("127.0.0.1:80")
         .expect("Couldn't connect to the server...");
     println!("Connectet");
     stream.write(echo_key).unwrap();
@@ -28,12 +27,16 @@ fn communication() -> Result<String, Error> {
     stream.shutdown(Shutdown::Both).expect("shutdown call failed");
     thread::sleep(time::Duration::from_millis(5000));
     let s = "Hat geklappt";
-    Ok(s.parse().unwrap())
+    Ok()
 }
 
 fn main() {
-    match communication() {
-        Ok(t) => println!("We've time travelled to !!"),
-        Err(..) => eprintln!("Oh noes, we don't know which era we're in! :( \n "),
+    let mut i = 0;
+    while i < 5 {
+        match communication() {
+            Ok(t) => println!("We've time travelled to !!"),
+            Err(..) => eprintln!("Oh noes, we don't know which era we're in! :( \n "),
+        }
+        i = i + 1;
     }
 }
